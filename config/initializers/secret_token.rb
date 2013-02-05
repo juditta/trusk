fb6f035b1a4f568def5f4bfcb0a304cd406f0c2e
Judyta <judyta.kurdys@gmail.com>
2013-02-05 12:25:29 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trusk::Application.config.secret_token = '2315ad3a11927cf5443726282b0d7bb986b1074b63d072aa5c7c3855ade9980ac12a73c3e835ca13503f3da14b93737e3977ab20e97b4265c219b1cb70be3747'
