36c4d8842e476e28df89ee27c5d5abde86193ea4
Judyta <judyta.kurdys@gmail.com>
2013-02-05 17:37:47 +0100

    devise, cancan

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+	# before_filter :authenticate_user!, :except => [:index]
+	#load_and_authorize_resource
+# Access Current User
+  def index
+
+  	@pageclass = 'frontpage'
+
+  end
+end  
